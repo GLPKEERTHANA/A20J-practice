cdb084c9c5591d979f083959383d5f6d4590b354
GLPKEERTHANA <gadepalli.keerthana@gmail.com>
2020-09-28 08:58:58 +0530

    Add existing file

--- 1-presents.cpp
+++ 1-presents.cpp
@@ -1,1 +1,27 @@
-#include<iostream>
+#include <iostream>
+#include<queue>
+using namespace std;
+
+int main()
+{    
+    int n;
+    cin>>n;
+    priority_queue<pair<int,int>, vector<pair<int,int>>  , greater <pair<int,int>>> q; 
+    for(int i=0;i<n;i++)
+    {   int ch;
+        cin>>ch;
+        q.push(make_pair(ch,i+1));
+    }
+    while(!q.empty())
+    {
+      cout<<q.top().second<<" ";
+      q.pop();
+
+    }
+
+
+
+
+    return 0;
+}
+//Microsoft visual c++ 2010
